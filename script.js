24bdf2b02fb38ec9ebaab20311c62370861244a4
Pierre-Loup Garrigues <pierre-loupg@hotmail.fr>
2021-02-20 15:58:00 +0100

    site pas encore adaptatif

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
+const text = document.getElementById("text");
 
-
-const text = document.getElementById('text');
-
-const textArray = text.textContent.split('');
+const textArray = text.textContent.split("");
 //const textArray = text.innerHTML.split('');
 
 console.log(textArray);
 
-const newElement = document.createElement('h1');
-newElement.innerHTML = `${textArray.map(letter => `<span style="${randomVisibility()}">${letter}<span>`).join('')}`;
+const newElement = document.createElement("h1");
+newElement.innerHTML = `${textArray
+  .map((letter) => `<span style="${randomVisibility()}">${letter}<span>`)
+  .join("")}`;
 
-newElement.classList.add('letter');
+newElement.classList.add("letter");
 
 document.body.appendChild(newElement);
 
-function randomVisibility(){
-	return Math.random() < 0.5 ? 'visibility:hidden' : 'visibility:visible';
+function randomVisibility() {
+  return Math.random() < 0.5 ? "visibility:hidden" : "visibility:visible";
 }
+
+// partie carte
+
+let buttonEarth = document.getElementById("earthMap");
+var nav = document.querySelector(".nav");
+
+buttonEarth.onclick = function () {
+  nav.classList.toggle("menu_open");
+};
+
+// close menu
+
+let buttonCloseMenu = document.getElementById("closeMenu");
+
+buttonCloseMenu.onclick = function () {
+  nav.classList.toggle("menu_open");
+};
