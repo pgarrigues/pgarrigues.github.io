fb6ef706bcbb2b6225bd5adbb0712b9e5a29c9fa
Pierre-Loup Garrigues <pierre-loupg@hotmail.fr>
2021-02-22 11:25:29 +0100

    ajout des popups info

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+///////////////
+// Effet sur H1
+///////////////
+
 const text = document.getElementById("text");
 
 const textArray = text.textContent.split("");
-//const textArray = text.innerHTML.split('');
-
-console.log(textArray);
 
 const newElement = document.createElement("h1");
 newElement.innerHTML = `${textArray
@@ -18,19 +19,115 @@ function randomVisibility() {
   return Math.random() < 0.5 ? "visibility:hidden" : "visibility:visible";
 }
 
-// partie carte
+////////////////////
+// Ouverture du menu
+////////////////////
 
 let buttonEarth = document.getElementById("earthMap");
-var nav = document.querySelector(".nav");
+let nav = document.querySelector(".nav");
 
 buttonEarth.onclick = function () {
   nav.classList.toggle("menu_open");
 };
 
-// close menu
+////////////////////
+// Fermeture du menu
+////////////////////
 
 let buttonCloseMenu = document.getElementById("closeMenu");
 
 buttonCloseMenu.onclick = function () {
   nav.classList.toggle("menu_open");
+};
+
+//////////////////////////
+// Gestion des popups info
+//////////////////////////
+
+///// popup cluster potagers :
+
+let popUpClusterPotagers = document.getElementById("popUpclusterPotagers");
+
+let buttonClusterPotagers = document.getElementById("btnClusterPotagers");
+
+let buttonClosePopUpClusterPotagers = document.getElementById(
+  "closePopUpclusterPotagers"
+);
+
+buttonClusterPotagers.onclick = function () {
+  popUpClusterPotagers.classList.toggle("appear");
+};
+
+buttonClosePopUpClusterPotagers.onclick = function () {
+  popUpClusterPotagers.classList.toggle("appear");
+};
+
+///// popup mailles hexa :
+
+let popUpMaillesHexa = document.getElementById("popUpMaillesHexa");
+
+let buttonMaillesHexa = document.getElementById("btnMaillesHexa");
+
+let buttonClosePopUpMaillesHexa = document.getElementById(
+  "closePopUpMaillesHexa"
+);
+
+buttonMaillesHexa.onclick = function () {
+  popUpMaillesHexa.classList.toggle("appear");
+};
+
+buttonClosePopUpMaillesHexa.onclick = function () {
+  popUpMaillesHexa.classList.toggle("appear");
+};
+
+///// popup dual map :
+
+let popUpDualMap = document.getElementById("popUpDualMap");
+
+let buttonDualMap = document.getElementById("btnDualMap");
+
+let buttonClosePopUpDualMap = document.getElementById("closePopUpDualMap");
+
+buttonDualMap.onclick = function () {
+  popUpDualMap.classList.toggle("appear");
+};
+
+buttonClosePopUpDualMap.onclick = function () {
+  popUpDualMap.classList.toggle("appear");
+};
+
+///// popup earthquakes :
+
+let popUpEarthquakes = document.getElementById("popUpEarthquakes");
+
+let buttonEarthquakes = document.getElementById("btnEarthquakes");
+
+let buttonClosePopUpEarthquakes = document.getElementById(
+  "closePopUpEarthquakes"
+);
+
+buttonEarthquakes.onclick = function () {
+  popUpEarthquakes.classList.toggle("appear");
+};
+
+buttonClosePopUpEarthquakes.onclick = function () {
+  popUpEarthquakes.classList.toggle("appear");
+};
+
+///// popup VG :
+
+let popUpVendeeGlobe = document.getElementById("popUpVendeeGlobe");
+
+let buttonVendeeGlobe = document.getElementById("btnVendeeGlobe");
+
+let buttonClosePopUpVendeeGlobe = document.getElementById(
+  "closePopUpVendeeGlobe"
+);
+
+buttonVendeeGlobe.onclick = function () {
+  popUpVendeeGlobe.classList.toggle("appear");
+};
+
+buttonClosePopUpVendeeGlobe.onclick = function () {
+  popUpVendeeGlobe.classList.toggle("appear");
 };
